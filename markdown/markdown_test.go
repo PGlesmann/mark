44e0aae10914d4520b636e489e5ce7f887eab5cf
Paul.Glesmann <paul.glesmann@tui.com>
2025-05-04 23:23:31 +0200

    chore: remove test for droph1 and stripNewLines

--- markdown/markdown_test.go
+++ markdown/markdown_test.go
@@ -93,7 +93,7 @@ func TestCompileMarkdownDropH1(t *testing.T) {
 		}
 		var variant string
 		switch filename {
-		case "testdata/quotes.md", "testdata/header.md", "testdata/admonitions.md":
+		case "testdata/quotes.md", "testdata/header.md":
 			variant = "-droph1"
 		default:
 			variant = ""
@@ -137,7 +137,7 @@ func TestCompileMarkdownStripNewlines(t *testing.T) {
 		}
 		var variant string
 		switch filename {
-		case "testdata/quotes.md", "testdata/codes.md", "testdata/newlines.md", "testdata/macro-include.md", "testdata/admonitions.md":
+		case "testdata/quotes.md", "testdata/codes.md", "testdata/newlines.md", "testdata/macro-include.md":
 			variant = "-stripnewlines"
 		default:
 			variant = ""
